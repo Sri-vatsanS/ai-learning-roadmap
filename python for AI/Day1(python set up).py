c6415efa5fd5fa491a6b19ad91a1821017899f48
Sri-vatsanS <srivatsan6803@gmail.com>
2026-09-21 21:51:18 +0530

    This is the Day 1 for setting up of python
      - This is the setting up of python for AI
      - Instalation done and whole python basic for AI will be learnt here

---
+++ python for AI/Day1(python set up).py
@@ -0,0 +1,7 @@
+# This is the first documentation of my Python program
+# I started it because I want to learn Python for building a strong foundation in AI and machine learning 
+# This is the documentation of what I have learned on each Day of my learning journey ... so lets get started 
+
+# Date:21-09-2026
+
+print("Hello World")
